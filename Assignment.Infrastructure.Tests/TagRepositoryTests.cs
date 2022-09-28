7e480f989f46b9da6476174b6997a960b1db63c7
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-28 23:26:04 +0200

    made tests for business rules user and tag

--- Assignment.Infrastructure.Tests/TagRepositoryTests.cs
+++ Assignment.Infrastructure.Tests/TagRepositoryTests.cs
@@ -10,41 +10,36 @@ public class TagRepositoryTests : IDisposable
 
   public TagRepositoryTests()
   {
-     var connection = new SqliteConnection("Filename=:memory:");
-            connection.Open();
-            var build = new DbContextOptionsBuilder<KanbanContext>();
-            build.UseSqlite(connection);
-            var context = new KanbanContext(build.Options);
-            context.Database.EnsureCreated();
-      context.SaveChanges();  
-      _context = context;
-      _repository = new TagRepository(_context);
+    var connection = new SqliteConnection("Filename=:memory:");
+    connection.Open();
+    var build = new DbContextOptionsBuilder<KanbanContext>();
+    build.UseSqlite(connection);
+    var context = new KanbanContext(build.Options);
+    context.Database.EnsureCreated();
+
+    context.Tags.AddRange(
+      new Tag(){ Id = 8, Name = "Frontend"},
+      new Tag(){ Id = 9, Name = "backend"},
+      new Tag(){ Id = 10, Name = "any end"}
+    );
+    
+    context.SaveChanges();  
+    _context = context;
+    _repository = new TagRepository(_context);
   }
   [Fact]
-  public void Test_tags_can_delete_force()
-  {
-    //Arrange
+  public void Test_tags_can_delete_force() => _repository.Delete(8, true).Should().Be(Response.Deleted);
 
-    //Act
-    //Assert
-  }
 
   [Fact]
-  public void Test_tags_cannot_delete_return_Conflict()
-  {
-    //Arrange
+  public void Test_tags_cannot_delete_return_Conflict() => _repository.Delete(9, false).Should().Be(Response.Conflict);
 
-    //Act
-    //Assert
-  }
 
   [Fact]
   public void Test_creat_tag_already_exist_return_Conflict()
   {
-    //Arrange
-
-    //Act
-    //Assert
+    var (response, id) = _repository.Create(new TagCreateDTO("any end"));
+    response.Should().Be(Response.Conflict);
   }
 
   public void Dispose()
