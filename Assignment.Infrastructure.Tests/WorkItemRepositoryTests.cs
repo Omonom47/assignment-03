c91fe83eef4da58fef3854f96e65527efe1947ae
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-28 11:52:18 +0200

    done

--- Assignment.Infrastructure.Tests/WorkItemRepositoryTests.cs
+++ Assignment.Infrastructure.Tests/WorkItemRepositoryTests.cs
@@ -2,5 +2,22 @@ namespace Assignment.Infrastructure.Tests;
 
 public class WorkItemRepositoryTests
 {
+    [Fact]
+    public void Test_only_new_can_be_deleted()
+    {
+        //Arrange 
+        //Act
+        //Assert
+    }
+
+     [Fact]
+    public void Test_delete_active_state_should_be_removed()
+    {
+        //Arrange 
+        //Act
+        //Assert
+    }
+
+  
 
 }
