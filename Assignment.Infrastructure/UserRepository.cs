af8bcfad86d09b876a05fbd6c32ad3b2c2c41e73
Maxime Birkkjær Havez <maxih@hotmail.dk>
2022-09-28 16:35:49 +0200

    implemented Create in UserRepository

--- Assignment.Infrastructure/UserRepository.cs
+++ Assignment.Infrastructure/UserRepository.cs
@@ -2,9 +2,30 @@ namespace Assignment.Infrastructure;
 
 public class UserRepository : IUserRepository
 {
+    private readonly KanbanContext _kanban;
     public (Response Response, int UserId) Create(UserCreateDTO user)
     {
-        throw new NotImplementedException();
+        var entity = _kanban.Users.FirstOrDefault(k => k.Email == user.Email);
+
+        Response resp;
+
+
+        if (entity is null)
+        {
+            entity = new User { Name = user.Name, Email = user.Email };
+
+            _kanban.Add(entity);
+            _kanban.SaveChanges();
+            resp = Response.Created;
+
+        }
+        else
+        {
+            resp = Response.Conflict;
+        }
+
+        return (resp, entity.Id);
+
     }
 
     public Response Delete(int userId, bool force = false)
