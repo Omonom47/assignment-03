f9c09d159f1abc9884031db0bdc346fbcd28b5e5
Maxime Birkkjær Havez <maxih@hotmail.dk>
2022-09-29 15:14:36 +0200

    Implemented leftover methods from interface

--- Assignment.Infrastructure/WorkItemRepository.cs
+++ Assignment.Infrastructure/WorkItemRepository.cs
@@ -2,114 +2,146 @@ namespace Assignment.Infrastructure;
 
 public class WorkItemRepository : IWorkItemRepository
 {
-  private readonly KanbanContext _context;
+    private readonly KanbanContext _context;
 
-  public WorkItemRepository(KanbanContext context)
-  {
-    _context = context;
-  }
-  public (Response Response, int WorkItemId) Create(WorkItemCreateDTO workItem)
-  {
-    var entity = new WorkItem
+    public WorkItemRepository(KanbanContext context)
     {
-      Title = workItem.Title,
-      Description = workItem.Description,
-      State = State.New,
-      Tags = workItem.Tags
-    };
-
-    _context.WorkItems.Add(entity);
-    var response = Response.Created;
-    return (response, entity.Id);
-
-  }
-
-  public Response Delete(int workItemId)
-  {
-    var workItem = _context.WorkItems.FirstOrDefault(w => w.Id == workItemId);
-    Response response;
-
-    if (workItem is null)
+        _context = context;
+    }
+    public (Response Response, int WorkItemId) Create(WorkItemCreateDTO workItem)
     {
-      response = Response.NotFound;
-      return response;
+        var entity = new WorkItem
+        {
+            Title = workItem.Title,
+            Description = workItem.Description,
+            State = State.New,
+            Tags = workItem.Tags
+        };
+
+        _context.WorkItems.Add(entity);
+        var response = Response.Created;
+        return (response, entity.Id);
+
     }
 
-    switch (workItem.State)
+    public Response Delete(int workItemId)
     {
-      case State.New:
-        _context.WorkItems.Remove(workItem);
-        response = Response.Deleted;
-        break;
-      case State.Active:
-        workItem.State = State.Removed;
-        response = Response.Updated;
-        break;
-      case State.Resolved:
-      case State.Closed:
-      case State.Removed:
-        response = Response.Conflict;
-        break;
-      default:
-        response = Response.BadRequest;
-        break;
+        var workItem = _context.WorkItems.FirstOrDefault(w => w.Id == workItemId);
+        Response response;
+
+        if (workItem is null)
+        {
+            response = Response.NotFound;
+            return response;
+        }
+
+        switch (workItem.State)
+        {
+            case State.New:
+                _context.WorkItems.Remove(workItem);
+                response = Response.Deleted;
+                break;
+            case State.Active:
+                workItem.State = State.Removed;
+                response = Response.Updated;
+                break;
+            case State.Resolved:
+            case State.Closed:
+            case State.Removed:
+                response = Response.Conflict;
+                break;
+            default:
+                response = Response.BadRequest;
+                break;
+        }
+        _context.SaveChanges();
+        return response;
     }
-    _context.SaveChanges();
-    return response;
-  }
 
-  public WorkItemDetailsDTO Find(int workItemId)
-  {
-    throw new NotImplementedException();
-  }
-
-  public IReadOnlyCollection<WorkItemDTO> Read()
-  {
-    throw new NotImplementedException();
-  }
-
-  public IReadOnlyCollection<WorkItemDTO> ReadByState(State state)
-  {
-    throw new NotImplementedException();
-  }
-
-  public IReadOnlyCollection<WorkItemDTO> ReadByTag(string tag)
-  {
-    throw new NotImplementedException();
-  }
-
-  public IReadOnlyCollection<WorkItemDTO> ReadByUser(int userId)
-  {
-    throw new NotImplementedException();
-  }
-
-  public IReadOnlyCollection<WorkItemDTO> ReadRemoved()
-  {
-    throw new NotImplementedException();
-  }
-
-  public Response Update(WorkItemUpdateDTO workItem)
-  {
-    Response response;
-    var entity = _context.WorkItems.Find(workItem.Id);
-
-    if (entity is null)
+    public WorkItemDetailsDTO Find(int workItemId)
     {
-      response = Response.NotFound;
-      return response;
+        var workItem = _context.WorkItems.FirstOrDefault(w => w.Id == workItemId);
+
+        if (workItem is null)
+        {
+            return null;
+        }
+        else
+        {
+            return new WorkItemDetailsDTO(workItemId, workItem.Title, workItem.Description, DateTime.Today, workItem.AssignedTo.Name, workItem.Tags.ToArray(), workItem.State, DateTime.Now);
+        }
+
     }
-    else
+
+    public IReadOnlyCollection<WorkItemDTO> Read()
     {
-      entity.Title = workItem.Title;
-      entity.Description = workItem.Description ?? entity.Description;
-      entity.Tags = workItem.Tags ?? entity.Tags;
-      if (workItem is not null)
-      {
-        entity.State = workItem.State;
-      }
-      response = Response.Updated;
-      _context.SaveChanges();
+        var workItems = from w in _context.WorkItems
+                        orderby w.State
+                        select new WorkItemDTO(w.Id, w.Title, w.AssignedTo.Name, w.Tags.ToArray(), w.State);
+
+        return workItems.ToArray();
     }
-    return response;
-  }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadByState(State state)
+    {
+        var workItems = from w in _context.WorkItems
+                        where w.State == state
+                        select new WorkItemDTO(w.Id, w.Title, w.AssignedTo.Name, w.Tags.ToArray(), w.State);
+
+        return workItems.ToArray();
+    }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadByTag(string tag)
+    {
+        var workItems = from w in _context.WorkItems
+                        where w.Tags.Contains(tag)
+                        orderby w.State
+                        select new WorkItemDTO(w.Id, w.Title, w.AssignedTo.Name, w.Tags.ToArray(), w.State);
+
+        return workItems.ToArray();
+    }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadByUser(int userId)
+    {
+        var workItems = from w in _context.WorkItems
+                        where w.AssignedTo.Id == userId
+                        orderby w.State
+                        select new WorkItemDTO(w.Id, w.Title, w.AssignedTo.Name, w.Tags.ToArray(), w.State);
+
+        return workItems.ToArray();
+    }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadRemoved()
+    {
+        var workItems = from w in _context.WorkItems
+                        where w.State == State.Removed
+                        select new WorkItemDTO(w.Id, w.Title, w.AssignedTo.Name, w.Tags.ToArray(), w.State);
+
+        return workItems.ToArray();
+    }
+
+    public Response Update(WorkItemUpdateDTO workItem)
+    {
+        Response response;
+        var entity = _context.WorkItems.Find(workItem.Id);
+
+        if (entity is null)
+        {
+            response = Response.NotFound;
+            return response;
+        }
+        else
+        {
+            entity.Title = workItem.Title;
+            entity.Description = workItem.Description ?? entity.Description;
+            entity.Tags = workItem.Tags ?? entity.Tags;
+            if (workItem is not null)
+            {
+                entity.State = workItem.State;
+            }
+            response = Response.Updated;
+            _context.SaveChanges();
+        }
+        return response;
+    }
 }
