e14a6c4d94f71f1899a1b05aeea6d20ad99a53b9
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-29 17:14:13 +0200

    one test left

--- Assignment.Infrastructure.Tests/WorkItemRepositoryTests.cs
+++ Assignment.Infrastructure.Tests/WorkItemRepositoryTests.cs
@@ -11,30 +11,33 @@ public class WorkItemRepositoryTests : IDisposable
         var connection = new SqliteConnection("Filename=:memory:");
         connection.Open();
         var builder = new DbContextOptionsBuilder<KanbanContext>();
-        builder.UseSqlite();
+        builder.UseSqlite(connection);
         var context = new KanbanContext(builder.Options);
         context.Database.EnsureCreated();
-        context.WorkItems.AddRange(new WorkItem() { Id = 0, Title = "pink post it", State = State.Active },
-                                    new WorkItem() { Id = 1, Title = "blue post it", State = Core.State.New },
-                                    new WorkItem() { Id = 2, Title = "green post it", State = Core.State.Resolved },
-                                    new WorkItem() { Id = 3, Title = "green post it", State = Core.State.Closed },
-                                    new WorkItem() { Id = 4, Title = "green post it", State = Core.State.Removed });
-        context.Users.Add(new User() { Id = 5, Name = "Clara" });
+
+        context.WorkItems.AddRange(new WorkItem() { Id = 100, Title = "pink post it", State = State.Active },
+                                    new WorkItem() { Id = 11, Title = "blue post it", State = Core.State.New },
+                                    new WorkItem() { Id = 12, Title = "green post it", State = Core.State.Resolved },
+                                    new WorkItem() { Id = 13, Title = "red post it", State = Core.State.Closed },
+                                    new WorkItem() { Id = 14, Title = "purple post it", State = Core.State.Removed });
+       
+        //context.Users.Add(new User() { Id = 5, Name = "Clara" });
+       
         context.SaveChanges();
 
-        _context = context;
-        _repository = new WorkItemRepository(context);
+    _context = context;
+    _repository = new WorkItemRepository(context);
 
     }
     [Fact]
     public void Test_only_new_can_be_deleted()
     {
         //Arrange 
-        var response = _repository.Delete(1);
+        var response = _repository.Delete(11);
         response.Should().Be(Response.Deleted);
 
         //Act
-        var entity = _context.WorkItems.Find(1);
+        var entity = _context.WorkItems.Find(11);
 
         //Assert
         entity.Should().BeNull();
@@ -42,13 +45,18 @@ public class WorkItemRepositoryTests : IDisposable
     }
 
     [Fact]
-    public void Test_delete_active_state_should_be_removed() => _repository.Find(0).Should().Be(State.Removed);
-    // {
-    //     var response =  _repository.Delete(0);
-    // }
+    public void Test_delete_active_state_should_be_removed() 
+    {
+        var response =  _repository.Delete(100);
+        
+        var entity = _context.WorkItems.Find(100);
+
+        entity.State.Should().Be(State.Removed);
+
+    }
 
     [Fact]
-    public void Test_delete_resolved_return_conflict() => _repository.Delete(2).Should().Be(Response.Conflict);
+    public void Test_delete_resolved_return_conflict() => _repository.Delete(12).Should().Be(Response.Conflict);
     // {
     //     //Arrange 
     //     var response =  _repository.Delete(1);
@@ -63,10 +71,10 @@ public class WorkItemRepositoryTests : IDisposable
     // }
 
     [Fact]
-    public void Test_delete_closed_return_conflict() => _repository.Delete(3).Should().Be(Response.Conflict);
+    public void Test_delete_closed_return_conflict() => _repository.Delete(13).Should().Be(Response.Conflict);
 
     [Fact]
-    public void Test_delete_removed_return_conflict() => _repository.Delete(4).Should().Be(Response.Conflict);
+    public void Test_delete_removed_return_conflict() => _repository.Delete(14).Should().Be(Response.Conflict);
 
     [Fact]
     public void create_should_return_new_and_created_current_time()
@@ -76,11 +84,7 @@ public class WorkItemRepositoryTests : IDisposable
         _repository.Find(5).Should().Be(State.New);
     }
 
-    [Fact]
-    public void assign_not_excisting_user_should_return_badrequest()
-    {
-
-    }
+    
 
 
     public void Dispose()
